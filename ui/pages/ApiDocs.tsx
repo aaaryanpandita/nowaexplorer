8071319a4bfc6ee690080637899fc6bc38914437
aryan <aryan@tarality.com>
2026-01-05 14:16:12 +0530

    comment_api

--- ui/pages/ApiDocs.tsx
+++ ui/pages/ApiDocs.tsx
@@ -17,9 +17,6 @@ const ApiDocs = () => {
 
   const tabs: Array<TabItemRegular> = [
     { id: 'rest_api', title: 'REST API', component: <RestApi/> },
-    { id: 'eth_rpc_api', title: 'ETH RPC API', component: <EthRpcApi/> },
-    { id: 'rpc_api', title: 'RPC API endpoints', component: <RpcApi/> },
-    { id: 'graphql_api', title: 'GraphQL API', component: <GraphQL/> },
   ].filter(({ id }) => feature.isEnabled && feature.tabs.includes(id));
 
   return (
